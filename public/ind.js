d3f29cff118c4396101b2f7e48363c5326939717
Yousaf912 <yousafva9@gmail.com>
2024-03-26 14:27:41 +0500

    updated

---
+++ public/ind.js
@@ -0,0 +1,44 @@
+var experience = document.getElementById('experience')
+var second = document.getElementById('secondH')
+function calculateExperience() {
+    var currentDate = new Date();
+    var startDate = new Date('2013-01-01');
+    var difference = currentDate - startDate;
+    var years = difference / (1000 * 60 * 60 * 24 * 365);
+    years = Math.round(years)
+    return years;
+}
+var experienceYears = calculateExperience();
+experience.innerHTML = experienceYears
+second.innerHTML = experienceYears;
+
+var name1 = document.getElementById('name').value
+var mail = document.getElementById('mail').value
+var message = document.getElementById('message').value
+var date = document.getElementById('date').value
+
+function sendMail() {
+    if (name1 === "" || mail === "" || message === "" || date === "") {
+        alert("Please Type SomeThing")
+    } else {
+        const serviceId = "service_681vp7r";
+        const templateId = "template_ea0vxde";
+
+        emailjs.send(serviceId, templateId, {
+            name: document.getElementById('name').value,
+            mail: document.getElementById('mail').value,
+            message: document.getElementById('message').value,
+            date: document.getElementById('date').value,
+        }).then(function (res) {
+            document.getElementById('name').value = "";
+            document.getElementById('mail').value = "";
+            document.getElementById('message').value = "";
+            document.getElementById('date').value = "";
+            console.log(res);
+            alert('Your message has been sent to Khalid Haider')
+        }).catch(function (eror) {
+            console.log(eror.message);
+        })
+    }
+}
+
