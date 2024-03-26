d273ef8962a3c93fd855e85b0b8606389fe5626c
Yousaf912 <yousafva9@gmail.com>
2024-03-26 14:49:05 +0500

    updated

--- public/ind.js
+++ public/ind.js
@@ -12,33 +12,37 @@ var experienceYears = calculateExperience();
 experience.innerHTML = experienceYears
 second.innerHTML = experienceYears;
 
-var name1 = document.getElementById('name').value
-var mail = document.getElementById('mail').value
-var message = document.getElementById('message').value
-var date = document.getElementById('date').value
 
-function sendMail() {
-    if (name1 === "" || mail === "" || message === "" || date === "") {
-        alert("Please Type SomeThing")
-    } else {
-        const serviceId = "service_681vp7r";
-        const templateId = "template_ea0vxde";
 
-        emailjs.send(serviceId, templateId, {
-            name: document.getElementById('name').value,
-            mail: document.getElementById('mail').value,
-            message: document.getElementById('message').value,
-            date: document.getElementById('date').value,
-        }).then(function (res) {
-            document.getElementById('name').value = "";
-            document.getElementById('mail').value = "";
-            document.getElementById('message').value = "";
-            document.getElementById('date').value = "";
-            console.log(res);
-            alert('Your message has been sent to Khalid Haider')
-        }).catch(function (eror) {
-            console.log(eror.message);
-        })
-    }
-}
+        function sendMail() {
+            var name = document.getElementById('name').value;
+            var mail = document.getElementById('mail').value;
+            var message = document.getElementById('message').value;
+            var date = document.getElementById('date').value;
+        
+            if (name === "" || mail === "" || message === "" || date === "") {
+                alert("Please type something in all fields.");
+            } else {
+                const serviceId = "service_681vp7r";
+                const templateId = "template_ea0vxde";
+        
+                emailjs.send(serviceId, templateId, {
+                    name: name,
+                    mail: mail,
+                    message: message,
+                    date: date
+                }).then(function(response) {
+                    document.getElementById('name').value = "";
+                    document.getElementById('mail').value = "";
+                    document.getElementById('message').value = "";
+                    document.getElementById('date').value = "";
+                    console.log("Email sent successfully:", response);
+                    alert('Your message has been sent to Khalid Haider.');
+                }).catch(function(error) {
+                    console.error("Email sending failed:", error);
+                    alert("An error occurred while sending your message. Please try again later.");
+                });
+            }
+        }
+        
 
